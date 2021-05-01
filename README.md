97be3a7119c5d952b637480bf766faebc40d6877
Jannik <jannik.sturhann@haw-hamburg.de>
2021-05-01 23:13:32 +0200

    workspace export

---
+++ README.md
@@ -0,0 +1,45 @@
+# Experiment mit MQTT und ESP32
+
+In diesem Experiment wurde eine Mosquitto (MQTT) Broker auf einem Ubuntu System aufgesetzt.
+Auf dem Ubuntu System wurde ein Subscriber mittels mosquitto_sub ausgesetzt, um die eigehenden Nachrichten empfangen zu können.
+
+Auf einem ESP32 wurde ein Erschütterungssensor, mittels Piezo Element und ein HC-SR04 Abstandssensor aufgesetzt. 
+Dabei sendet der Erschütterungssensor eine Nachricht mit dem Topic "esp32/tap", wenn eine Erschütterung erkannt wurde.
+Der Abstandssensor ist so konfiguriert, dass die Mikrosekunden gezählt werden, die zwischen einem emmitiertem Ultraschallimpuls und dem Empfang selbigem vergangen sind.
+Es werden 10 Messungen vorgenommen. Die Messwerte werden dann aufsteigend sortiert und es wird die differenz zwischen dem größten und dem kleinsten Messwert genommen.
+Ist die Differenz größer als ein definierter Treshold, wird eine Nachricht mit dem Topic "esp32/ultrasonic" und dem Differenzwert als Nachricht gesendet.
+Der Abstandssensor wird somit als Bewegungssensor verwendet.
+
+## Mosquitto Broker Installation auf Ubuntu 
+```
+apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
+apt-get update
+apt-get install mosquitto
+apt-get install mosquitto-clients
+apt clean
+```
+
+### Debug Subscriber, der alle Nachrichten abfängt
+  - ```-h``` für host
+  - ```-v``` für verbose outout
+  - ```-t``` topic, dass abboniert werden soll. '#' als Wildcard für alle Topics
+  - Command: ```mosquitto_sub -h localhost -v -t "#"```
+
+### Senden einer lokalen Testnachricht "XXX" mit dem Topic "test/pling"
+  - ```-h``` für host
+  - ```-t``` für topic 
+  - ```-m``` für die gesendete Nachricht
+- Command: ```mosquitto_pub -h localhost -t "test/pling" -m "XXX"```
+
+## Client Setup auf dem ESP32
+Als ESP32 Modul wurde ein "ESP32 Dev Kit C V4" von Az-Delivery verwendet.
+Mittels der Arduino Library PubSubClient kann man komfortabel eine Verbindung zum Mosquitto Broker aufbauen. Dafür muss inb der Mosquitto config, der Broker nach außen geöffnet werden.
+In meinem Setup habe ich das Wlan Interface meines Laptops freigegeben, was mit hinzufügen der Zeile ```bind_interface wlp5s0``` in der Datei ```/etc/mosquitto/conf.d/``` möglich war. Das aktull verwendete Interface kann mit dem Befhl ```ip a``` herausgefunden werden.
+Nach dem Aktualsieren der Config muss der Service miot dem Befehl ```sudo service mosquitto restart```` neu gestartet werden.
+
+## Fazit 
+Sowohl der Erschütterungs-, sowie der Abstandssensor funktionierten gut in dem beschriebenen Setup. Die Nutzung des Abstandssensor als Bewegungssensor hat ebenfalls gut funktioniert. Dabei auf kurze Abstände bis ca. 50cm ohne Fehler. Bei längeren Abständen sind gelegentlich fehlerhafte Messwerte aufgetreten. 
+
+## Referenzen
+  - https://randomnerdtutorials.com/esp32-mqtt-publish-subscribe-arduino-ide/
+  - https://elektro.turanis.de/html/prj121/index.html
--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,115 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
 
-
+//shock sensor and led
 #define IN_PIN 12
 #define OUT_PIN 14
-#define DELAY 50
-
+#define DELAY 30
+volatile byte lastState = LOW;
 volatile byte state = LOW;
 volatile uint32_t lastInMillis;
+unsigned int numTaps = 0;
+
+//ultrasonic
+#define PIN_TRIGGER 27
+#define PIN_ECHO 26
+#define NUM_SAMPLES 10
+#define DIFF_THRESHOLD 100
+
+//networking
+#define WIFI_SSID "<wifi_ssid>"
+#define WIFI_PASSWORD "<wifi_password_here>"
+#define MQTT_SERVER "<hostname_of_mosquitto_server>"
+#define MQTT_PORT 1883
+char msg[64];
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi();
+void reconnect();
 void blink();
+unsigned long sampleUltrasonic();
 
 void setup() {
   Serial.begin(115200);
   pinMode(OUT_PIN,OUTPUT);
+  digitalWrite(OUT_PIN, LOW);
   pinMode(IN_PIN, INPUT);
+  pinMode(PIN_TRIGGER, OUTPUT);
+  pinMode(PIN_ECHO, INPUT);
   attachInterrupt(IN_PIN, blink, RISING);
+  setup_wifi();
+  client.setServer(MQTT_SERVER, MQTT_PORT);
 }
 
 void loop() {
-  digitalWrite(OUT_PIN, state);
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long ultrasonicDiff = sampleUltrasonic();
+  if(ultrasonicDiff > DIFF_THRESHOLD) {
+    sprintf(msg, "%lu", ultrasonicDiff);
+    client.publish("esp32/ultrasonic", msg);
+    delay(20);
+  }
+
+  if(state != lastState) {
+    lastState = state;
+    digitalWrite(OUT_PIN, state);
+    sprintf(msg, "tap %u received", ++numTaps);
+    client.publish("esp32/tap", msg);
+  }
 }
 
 void blink() {
-  // Invertiere den Status: "Lass die LED blinken von HIGH auf LOW/ an auf aus"
   if (millis() > lastInMillis + DELAY || lastInMillis > millis()) {
     state = !state;
-    Serial.println("change");
     lastInMillis = millis();
   }
 }
 
-
-/*
-using namespace std;
-
-#define NUM_SAMPLES 15
-#define MEDIAN  NUM_SAMPLES/2
-
-void setup() {
-  Serial.begin(115200);
-  delay(1000); 
+void setup_wifi() {
+  Serial.printf("Connecting to %s", WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.printf("WiFi connected\nIP address: %s\n", WiFi.localIP().toString().c_str());
 }
 
-void loop() {
-  uint16_t touch1[NUM_SAMPLES];
-  uint16_t touch2[NUM_SAMPLES];
-  for (uint32_t i = 0; i<NUM_SAMPLES; i++) {
-    touch1[i] = touchRead(12);
-    touch2[i] = touchRead(14);
-    delay(1);
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP32_Client")) {
+      Serial.println("MQTT connected");
+    } else {
+      Serial.printf("failed, rc=%d\n", client.state());
+      Serial.println("Trying again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+unsigned long sampleUltrasonic()
+{
+  unsigned long samples[NUM_SAMPLES];
+  for (int i = 0; i < NUM_SAMPLES; i++)
+  {
+    digitalWrite(PIN_TRIGGER, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(PIN_TRIGGER, HIGH);
+    delayMicroseconds(10);
+
+    samples[i] = pulseIn(PIN_ECHO, HIGH);
+    delay(10);
   }
 
-  sort(touch1, touch1 + NUM_SAMPLES);
-  sort(touch2, touch2 + NUM_SAMPLES);
-
-  Serial.printf("[G12: %d, G14: %d]\n", touch1[MEDIAN], touch2[MEDIAN]); 
-  delay(20);
+  std::sort(samples, samples + NUM_SAMPLES);
+  return samples[NUM_SAMPLES - 1] - samples[0];
 }
-*/
